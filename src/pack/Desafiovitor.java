3503e264b95c6521fac4e4310a0aa2785c00f221
Victor Teixeira <victor.teixeira@tmaior.com.br>
2023-03-09 14:35:49 -0300

    de int para var

--- src/pack/Desafiovitor.java
+++ src/pack/Desafiovitor.java
@@ -4,7 +4,7 @@ public class Desafiovitor {
 
 	public static void main (String[] args) {
 		
-		int zero = 0;
+		var zero = 0;
 		for( zero = 1; zero <= 99; zero++) {
 			if (zero % 10 == 0) {
 		        continue;
