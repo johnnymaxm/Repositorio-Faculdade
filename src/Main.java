c56938095d2cb4d4be0eaa0d3ed755f1b916b3ec
johnnymaxm <johnnymax_10@hotmail.com>
2023-03-20 21:39:32 -0300

    consertando

--- src/Main.java
+++ src/Main.java
@@ -8,92 +8,65 @@ public class Main {
          * caracteres especiais) validar os numero Não menor que 0 e não maior que 100 E
          * apresentar mensagem de erro
          */
-
         Scanner scan = new Scanner(System.in);
 
         // Validação do nome
         System.out.println("Digite seu nome: ");
         var firstName = scan.nextLine();
 
-        var isNomeValido = validacoesNome(firstName); // false
-        if(!isNomeValido){
-            System.out.print("DJSKALDJASLJKDSA");
-            return;
-        }
-
         // Validação do sobrenome
         System.out.println("Digite seu sobrenome: ");
         var lastName = scan.nextLine();
 
-        var isSobrenomeValido = validacoesSobrenome(lastName);
-        if(!isSobrenomeValido){
-            System.out.print("DJSKALDJASLJKDSA");
-            return;
-        }
+        validacoesNome(firstName, lastName);
 
         // Validação dos numeros
         System.out.println("Digite um numero ");
         var userInput1 = scan.nextInt();
-        if ((userInput1 < 0 || userInput1 > 100)) {
-            System.out.println("Numero é melhor que 0 ou maior que 100");
-            return;
-        }
 
         System.out.println("Digite o segundo numero: ");
         var userInput2 = scan.nextInt();
-        if ((userInput2 < 0 || userInput2 > 100)) {
-            System.out.println("Numero é melhor que 0 ou maior que 100");
-            return;
-        }
 
         System.out.println("Digite o terceiro numero: ");
         var userInput3 = scan.nextInt();
-        if ((userInput3 < 0 || userInput3 > 100)) {
-            System.out.println("Numero é melhor que 0 ou maior que 100");
-            return;
-        }
+
+        validacoesNum(userInput1, userInput2, userInput3);
+
         System.out.println(firstName + " " + lastName);
         System.out.println("Numeros digitado: " + userInput1 + " " + userInput2 + " " + userInput3);
     }
 
-
-    public static boolean validacoesNome(String nome) {
+    public static boolean validacoesNome(String nome, String sobrenome) {
         if (nome.length() <= 2 || nome.length() >= 15) {
-            return false;
+            throw new IllegalArgumentException("O nome que você escolheu é muito curto, muito grande.");
         }
 
-//        if (sobrenome.length() <= 2 || sobrenome.length() >= 15) {
-//            return false;
-//        }
+        if (sobrenome.length() <= 2 || sobrenome.length() >= 15) {
+            throw new IllegalArgumentException("O sobrenome que você escolheu é muito curto, muito grande.");
+        }
 
         if (!Pattern.matches("[a-zA-Z]+", nome)) {
-            return false;
+            throw new IllegalArgumentException("O nome contém caracteres especiais.");
         }
 
-//        if (!Pattern.matches("[a-zA-Z]+", sobrenome)) {
-//            return false;
-//        }
-
+        if (!Pattern.matches("[a-zA-Z]+", sobrenome)) {
+            throw new IllegalArgumentException("O sobrenome contém caracteres especiais.");
+        }
         return true;
     }
 
-    public static boolean validacoesSobrenome(String sobrenome) {
-//        if (nome.length() <= 2 || nome.length() >= 15) {
-//            return false;
-//        }
-
-        if (sobrenome.length() <= 2 || sobrenome.length() >= 15) {
-            return false;
+    public static boolean validacoesNum(int num, int num2, int num3) {
+        if (num < 0 || num > 100) {
+            throw new IllegalArgumentException("O primeiro numero é menor que 0 ou maior que 100.");
         }
-
-//        if (!Pattern.matches("[a-zA-Z]+", nome)) {
-//            return false;
-//        }
-
-        if (!Pattern.matches("[a-zA-Z]+", sobrenome)) {
-            return false;
+        if (num2 < 0 || num2 > 100) {
+            throw new IllegalArgumentException("O segundo numero é menor que 0 ou maior que 100.");
+        }
+        if (num3 < 0 || num3 > 100) {
+            throw new IllegalArgumentException("O terceiro número é menor que 0 ou maior que 100.");
         }
 
         return true;
+
     }
 }
