68e4d658e58bbc41a238c38959871f1a7a62a89b
johnnymaxm <johnnymax_10@hotmail.com>
2023-04-11 22:10:50 -0300

    Oi tudo bem

--- src/Ex01.java
+++ src/Ex01.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 
 public class Ex01 {
     public static void main(String[] args) {
-        System.out.println("oi");
+        System.out.println("oi, tudo bem");
     }
 }
