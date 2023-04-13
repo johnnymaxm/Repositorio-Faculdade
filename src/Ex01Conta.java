362dacb3b7fb582bc4d44ae9b7d7577f1313e7fc
johnnymaxm <johnnymax_10@hotmail.com>
2023-04-13 20:59:08 -0300

    adicionando exercicio

--- src/Ex01Conta.java
+++ src/Ex01Conta.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
-public class Ex01 {
+public class Ex01Conta {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         String nome = "Johnny Max";
         String tipoConta = "Corrente";
         double saldo = 1500.00;
-        double transferencia = 0;
-        double depositar = 0;
+        double transferencia;
+        double depositar;
         int opcao = 0;
+
+
 
         System.out.println("Nome: " + nome);
         System.out.println("Tipo da conta: " + tipoConta);
         System.out.println("Saldo da conta: " + saldo + "\n");
 
         String menu = """
-                Digite sua opção
+                \nDigite sua opção
                 1 - Consultar saldo
                 2 - Transferir valor
                 3 - Receber valor
@@ -25,28 +27,29 @@ public class Ex01 {
 
         //Selecionar opções
         while (opcao != 4) {
+
+
             System.out.println(menu);
             opcao = scan.nextInt();
 
             //Consultar saldo
             if (opcao == 1) {
                 System.out.println("Seu saldo é: R$" + saldo);
-                break;
+                continue;
             }
 
             //Fazer transferencia
             if (opcao == 2) {
-                System.out.println("Digite valor que ira transferir: ");
+                System.out.print("Digite valor que ira transferir: ");
                 transferencia = scan.nextDouble();
 
 
                 if (saldo >= transferencia) {
-                    System.out.println("Valor transferido: " + (transferencia) + "\nSaldo atual da conta: " + (transferencia - saldo));
-                    break;
+                    System.out.println("Valor transferido: " + (transferencia) + "\nSaldo atual da conta: " + (saldo -  transferencia));
                 } else {
                     System.out.println("Saldo insuficiente");
-                    break;
                 }
+                continue;
             }
 
 
@@ -54,15 +57,14 @@ public class Ex01 {
             if (opcao == 3) {
                 System.out.println("Digite valor que deseja depositar: ");
                 depositar = scan.nextDouble();
-                System.out.println("Valor depositado: R$" + depositar + "\nValor atual da conta: R$" + (saldo + depositar));
-                break;
+                System.out.println("Valor depositado: R$" + depositar + "\nValor atual da conta: R$" + (saldo +  depositar));
+                continue;
             }
             if (opcao > 4) {
                 System.out.println("Opção invalida");
             }
+            System.out.println("Atendimento encerrado");
+
         }
-        //opção de sair
-        System.out.println("Atendimento encerrado");
     }
 }
-
